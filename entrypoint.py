23b4e0cf9ed9eafeaa69838c08202ed070d9ee9c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-05-21 17:50:28 +0200

    add: entrypoint main

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+import logging
+import sys
+from classes.bot import Bot
 
 def main():
-    pass
+    try:        
+        logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
+        bot = Bot()
+        bot.communicate()
+        
+    except KeyboardInterrupt:
+        logging.info("exiting")
+        sys.exit(0)
+        
+    except Exception as e:
+        logging.error(f"{e=}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
